8347c573da3017e710029b85704a0e96c785616a
Alexey Agapitov <marwinxxii@gmail.com>
2012-09-05 01:28:06 +0400

    changed app key

--- app/store/Flickr.js
+++ app/store/Flickr.js
@@ -6,7 +6,7 @@ Ext.define('demo.store.Flickr', {
             type: 'jsonp',
             url: 'http://api.flickr.com/services/rest/',//api_sig=483d69c418eaf235f3a723ba017998a2',
             extraParams: {
-                api_key: 'f962dae2d1d11a1d44d6f88d6a2301a9',
+                api_key: 'e0b8fe486f491a77fc18b5c3ecc6c55b',
                 method: 'flickr.interestingness.getList',
                 format: 'json',
             },
